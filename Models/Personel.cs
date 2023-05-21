51c485c8997dfab0000ca6e9010dde0a4cb749b1
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-05-21 05:43:49 +0300

    Refactoring

---
+++ Models/Personel.cs
@@ -0,0 +1,8 @@
+﻿namespace OrnekUygulamaMVC.Models
+{
+    public class Personel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ ViewComponents/PersonellerViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OrnekUygulamaMVC.Models;
+
+namespace OrnekUygulamaMVC.ViewComponents
+{
+    public class PersonellerViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<Personel> datas = new List<Personel>
+            {
+                new Personel(){Name = "Şuaip",Surname = "Abi"},
+                new Personel(){Name = "Şakip",Surname = "Bey"},
+                new Personel(){Name = "Muhdin",Surname = "Yılmaz"},
+            };
+
+            return View(datas);
+        }
+    }
+}
